30a2df1d55f5a3dbdc5bfc57232685ad99299caf
Christian Fernandez <chr.edg.fer@hotmail.com>
2023-11-06 21:48:24 -0300

    Actualizacion desde Notebook

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./body.css";
 import CardOk from "../card/CardOk";
 import CardError from "../card/CardError";
-import { Card, Form, Button } from "react-bootstrap";
+import { Card, Form, Button, FormGroup } from "react-bootstrap";
 import QRCode from "react-qr-code";
 
 const Body = () => {
@@ -41,6 +41,25 @@ const Body = () => {
     }
   };
 
+  const printContent = useRef(null);
+
+  const handlePrint = () => {
+    const content = printContent.current;
+    const printWindow = window.open("", "_blank");
+    printWindow.document.open();
+    printWindow.document.write(`
+      <html>
+        <head>
+          <title>Etiqueta QR</title>
+        </head>
+        <body onload="window.print(); window.close();">
+          ${content.innerHTML}
+        </body>
+      </html>
+    `);
+    printWindow.document.close();
+  };
+
   return (
     <div className="margenSup container d-flex justify-content-center">
       <Card className="cardRepuestos shadow">
@@ -55,12 +74,11 @@ const Body = () => {
                 name="partNumber"
                 value={formData.partNumber}
                 onChange={handleChange}
+                className={show && error && "text-danger"}
               />
-              {show && error && (
-                <Form.Text className="text-danger">
-                  Debe tener al menos 3 caracteres y no comenzar con espacio.
-                </Form.Text>
-              )}
+              <Form.Text className={show && error && "text-danger"}>
+                Debe tener al menos 3 caracteres y no comenzar con espacio.
+              </Form.Text>
             </Form.Group>
             <Form.Group controlId="description" className="formGroup">
               <Form.Label>Descripción</Form.Label>
@@ -70,19 +88,25 @@ const Body = () => {
                 name="description"
                 value={formData.description}
                 onChange={handleChange}
+                className={show && error && "text-danger"}
               />
-              {show && error && (
-                <Form.Text className="text-danger">
-                  Debe tener al menos 6 caracteres.
-                </Form.Text>
-              )}
+              <Form.Text className={show && error && "text-danger"}>
+                Debe tener al menos 6 caracteres.
+              </Form.Text>
             </Form.Group>
+            <FormGroup className="formBotones">
             <Button variant="primary" type="submit" className="float-right">
-              Enviar
+                Generar
             </Button>
+            {show && !error && 
+              <Button variant="primary" onClick={handlePrint} className="float-right">
+                Imprimir
+              </Button>
+            }
+            </FormGroup>
           </Form>
         </Card.Body>
-        <Card.Footer className="text-muted">
+        <Card.Footer className="text-muted formFooter">
           {show && (
             <>
               {error && <CardError error={error} />}
--- src/components/card/CardError.jsx
+++ src/components/card/CardError.jsx
@@ -4,7 +4,7 @@ import "./cardError.css";
 const CardError = ({ error }) => {
   return (
     <div className="cardError">
-      <h5>Error</h5>
+      <h5 className="text-danger">Error</h5>
       <p>Descripción: {error}</p>
     </div>
   );
